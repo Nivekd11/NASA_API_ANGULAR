c3ef4f8abc7c56ebb4cc2e3e9e6e411448e899b2
Nivekd11 <kevinsandoval06@gmail.com>
2022-09-28 16:50:47 -0500

    Detalles de estilo y CanActive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
     path:'dashboard',
     loadChildren: ()=> import('./modules/dashboard/dashboard.module').then(m=>m.DashboardModule),
     canLoad: [AuthGuard],
-    //canActivate: [AuthGuard]
+    canActivate: [AuthGuard]
   },
   
   {
